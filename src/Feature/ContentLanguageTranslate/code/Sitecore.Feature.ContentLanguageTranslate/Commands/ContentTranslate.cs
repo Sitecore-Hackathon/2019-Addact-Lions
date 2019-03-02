680de590cdcc4fe9a73a457a48fae08852e1c970
Mitesh Patel <patelmitesh1688@gmail.com>
2019-03-02 11:55:45 +0530

    added model file and sitecore item validation for command

--- src/Feature/ContentLanguageTranslate/code/Sitecore.Feature.ContentLanguageTranslate/Commands/ContentTranslate.cs
+++ src/Feature/ContentLanguageTranslate/code/Sitecore.Feature.ContentLanguageTranslate/Commands/ContentTranslate.cs
@@ -1,8 +1,15 @@
-﻿using Sitecore.Shell.Framework.Commands;
+﻿using Sitecore.Data;
+using Sitecore.Shell.Framework.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Sitecore.Feature.ContentLanguageTranslate.Models;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+using Sitecore.Diagnostics;
+using System.Collections.Specialized;
+using Sitecore.Web.UI.Sheer;
 
 namespace Sitecore.Feature.ContentLanguageTranslate.Commands
 {
@@ -10,7 +17,44 @@ namespace Sitecore.Feature.ContentLanguageTranslate.Commands
     {
         public override void Execute(CommandContext context)
         {
+            try
+            {
+                Database dbweb = Database.GetDatabase(CTConfiguration.SitecoreMasterDatabase);
+                Item configurationitem = dbweb.GetItem(new ID(CTConfiguration.ItemId));
+                CheckboxField __enableCT = configurationitem.Fields[CTConfiguration.EnableCT];
+                if (__enableCT.Checked)
+                {
+                    Assert.ArgumentNotNull(context, "context");
 
+                    if (context.Items.Length == 1)
+                    {
+                        NameValueCollection parameters = new NameValueCollection();
+                        parameters["items"] = base.SerializeItems(context.Items);
+                        parameters.Add(context.Parameters);
+                        Context.ClientPage.Start(this, "Run", parameters);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("ContentTranslate -Execute - " + ex.Message, typeof(ContentTranslate));
+            }
+        }
+
+        protected void Run(ClientPipelineArgs args)
+        {
+            try
+            {
+                Item item = base.DeserializeItems(args.Parameters["items"])[0];
+                if (item == null)
+                {
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("ContentTranslate - Run - " + ex.Message, typeof(ContentTranslate));
+            }
         }
     }
 }
---
+++ src/Feature/ContentLanguageTranslate/code/Sitecore.Feature.ContentLanguageTranslate/Models/CTConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.ContentLanguageTranslate.Models
+{
+    public class CTConfiguration
+    {
+        public const string ItemId = "{2A666E8F-868D-4607-8438-1481FCF709ED}";
+        public const string AccessKey = "Access Key";
+        public const string SitecoreWebDatabase = "web";
+        public const string SitecoreMasterDatabase = "master";
+        public const string EnableCT = "EnableCT";
+    }
+}
