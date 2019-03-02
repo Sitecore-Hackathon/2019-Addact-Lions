cc2f9a245bbc4beb720afdd190160446f5d64363
Mitesh Patel <patelmitesh1688@gmail.com>
2019-03-02 11:31:06 +0530

    Revert "Revert "created new project and added sitecore reference.""
    This reverts commit 2aba0a21dbde0a692f341c3811b67e176be40ac4.

---
+++ src/Feature/ContentLanguageTranslate/code/Sitecore.Feature.ContentLanguageTranslate/Commands/ContentTranslate.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Shell.Framework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.ContentLanguageTranslate.Commands
+{
+    public class ContentTranslate : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+
+        }
+    }
+}
